c56efc6a4fb133a75ac6bab9a90ddc02ea67d91d
Kevin de Haan <kbehaan@gmail.com>
2020-04-25 22:11:21 -0600

    hotels working wtf

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
 import csv
+import sys
 # import sklearn
 from sklearn.linear_model import SGDClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn import svm
 from sklearn import metrics
 from sklearn import dummy
+from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
@@ -24,12 +26,17 @@ def readCSV():
     
 
     end_idx = data.shape[1]-1
-    for i in range(end_idx):
-        le = preprocessing.LabelEncoder()
-        le.fit(data[:,i])
-        data[:,i] = le.transform(data[:,i])
+    for i in range(end_idx+1):
+        try:
+            data[:,i] = data[:,i].astype(np.float64)
+        except:
+            le = preprocessing.LabelEncoder()
+            le.fit(data[:,i])
+            data[:,i] = le.transform(data[:,i])
+        
+    data = data.astype(np.float64)
 
-    data = data.astype(np.float)
+    print("data", data[:3])
 
     x_data, t_data = data[:, 0:end_idx], data[:, end_idx]
     
@@ -48,15 +55,19 @@ def readCSV():
 
 
 def main():
+    search = False
+    if len(sys.argv) > 0:
+        if "-s" in sys.argv:
+            search = True
     x_train, t_train, x_test, t_test, x_val, t_val, _ = readCSV()
 
     # svm_classifier = get_svm(x_train, t_train, x_val, t_val)
     # print("SVM tested at", validate_cross(svm_classifier, x_test, t_test))
 
-    sgd_classifier = get_sgd(x_train, t_train, x_val, t_val)
+    sgd_classifier = get_sgd(x_train, t_train, x_val, t_val, search)
     print("SGD tested at", validate_cross(sgd_classifier, x_test, t_test))
 
-    mlp_classifier = get_mlp(x_train, t_train, x_val, t_val)
+    mlp_classifier = get_mlp(x_train, t_train, x_val, t_val, search)
     print("MLP tested at", validate_cross(mlp_classifier, x_test, t_test))
 
     majority_guess = get_majority(x_train, t_train)
@@ -66,20 +77,64 @@ def main():
     print("Uniform tested at", validate_cross(uniform_guess, x_test, t_test))
 
 
-def get_svm(x_train, t_train, x_val, t_val):
-    svm_classifier = svm.SVC()
-    svm_classifier.fit(x_train, t_train)
-    print("svm validated at", validate_cross(svm_classifier, x_val, t_val))
-    return svm_classifier
+# def get_svm(x_train, t_train, x_val, t_val):
 
-def get_sgd(x_train, t_train, x_val, t_val):
-    sgd_classifier = SGDClassifier(loss="hinge", penalty="l2", max_iter=1000)
+#     svm_params = svc_param_sel(x_train, t_train, 5)
+#     print("svm params", svm_params)
+#     svm_classifier = svm.SVC()
+#     svm_classifier.fit(x_train, t_train)
+#     print("svm validated at", validate_cross(svm_classifier, x_val, t_val))
+#     return svm_classifier
+
+# def svc_param_sel(x, y, nfolds):
+#     Cs = [0.001, 0.01, 0.1, 1, 10]
+#     gammas = [0.001, 0.01, 0.1, 1]
+#     param_grid = {'C': Cs, 'gamma' : gammas}
+#     grid_search = GridSearchCV(svm.SVC(kernel='rbf'), param_grid, cv=nfolds)
+#     grid_search.fit(x, y)
+#     grid_search.best_params_
+#     return grid_search.best_params_
+
+
+def param_sel(x, y, model, params):
+    print("Running GridSearch on hyperparameters:", params)
+    grid_search = RandomizedSearchCV( model, param_distributions=params, n_jobs=-1, cv=3)
+    grid_search.fit(x, y)
+    grid_search.best_params_
+    return grid_search.best_params_
+
+def get_sgd(x_train, t_train, x_val, t_val, search=False):
+    # {'alpha': 0.1, 'loss': 'hinge', 'penalty': 'l2'}
+    if search:
+        sgd_params = param_sel(x_train, t_train, SGDClassifier(max_iter=2000), {
+            'alpha': [0.0001, 0.001, 0.01, 0.1, 1],
+            'loss': ['hinge', 'log', 'squared_hinge',],
+            'penalty': ['l2'] })
+    else:
+        sgd_params = {'alpha': 0.1, 'loss': 'hinge', 'penalty': 'l2'}
+
+    print("params", sgd_params)
+    sgd_classifier = SGDClassifier(**sgd_params, max_iter=2000)
     sgd_classifier.fit(x_train, t_train)
     print("sgd validated at", validate_cross(sgd_classifier, x_val, t_val))
     return sgd_classifier
 
-def get_mlp(x_train, t_train, x_val, t_val):
-    mlp_classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+
+def get_mlp(x_train, t_train, x_val, t_val, search=False):
+    # {'activation': 'relu', 'alpha': 0.1, 'learning_rate': 'constant', 'solver': 'adam'}
+    if search:
+
+        mlp_params = param_sel(x_train, t_train, MLPClassifier(max_iter=1000), {
+            'alpha': [ 0.001, 0.006, 0.01, 0.06, 0.1],
+            'hidden_layer_sizes': [(50,50,50), (50,100,50), (100,)],
+            'activation': ['relu', 'tanh'],
+            'solver': ['sgd', 'adam'],
+            'learning_rate': ['constant', 'adaptive'], })
+    else:
+        mlp_params = {'activation': 'relu', 'alpha': 0.6, 'learning_rate': 'adaptive', 'solver': 'adam', 'hidden_layer_sizes': (20,20,10), 'verbose': True, "n_it"}
+
+    print("MLP params:", mlp_params)
+    mlp_classifier = MLPClassifier(**mlp_params, max_iter=6000)
     mlp_classifier.fit(x_train, t_train)
     print("mlp validated at", validate_cross(mlp_classifier, x_val, t_val))
     return mlp_classifier
@@ -100,7 +155,7 @@ def get_uniform(x_train, t_train):
 #     return auprc
 
 def validate_cross(classifier, x, y):
-    return cross_val_score(classifier, x, y, cv=5)
+    return (cross_val_score(classifier, x, y, cv=5), accuracy_score(y, classifier.predict(x)))
 
 if __name__ == "__main__":
     main()
